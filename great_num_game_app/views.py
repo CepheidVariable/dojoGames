85b9285e7dd2b6c131b0b002c9305d6ab8296114
CepheidVariable <robertwoodard.usa@gmail.com>
2021-01-13 00:59:59 -0600

    leaderboard backend finished

--- great_num_game_app/views.py
+++ great_num_game_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from random import randint
+import datetime
+import json
 
 
 # Create your views here.
@@ -8,18 +10,16 @@ def great_num_game(request):
         request.session['random'] = randint(1, 100)
         request.session['attempts'] = 0
         request.session['guess_result'] = "idle"
-    print(request.session['random'], request.session['attempts'])
     if request.session['attempts'] == 10:
-        print("Sorry, you loose.....")
         request.session['guess_result'] = "lost"
 
+    print(request.session['random'], request.session['attempts'])
     return render(request, 'number_game.html')
 
 
 def guess(request):
     #check if valid input or lost
     if (not request.POST['guess'].isnumeric()):
-        print("Invalid guess. Try a number between 1-100.")
         return redirect('/greatnumbergame')
     
     # increase attempt counter
@@ -27,22 +27,18 @@ def guess(request):
 
     #determine if guess is correct
     if int(request.POST['guess']) == request.session['random']:
-        print("Correct Guess!")
         request.session['guess_result'] = "success"
         return redirect('/greatnumbergame')
 
     elif(int(request.POST['guess']) < (request.session['random'] + 6) and int(request.POST['guess']) > (request.session['random'] - 6)):
-        print("Ooo, close! Try again.")
         request.session['guess_result'] = "near"
         return redirect('/greatnumbergame')
 
     elif (int(request.POST['guess']) < request.session['random']):
-        print("Too low! Try again.")
         request.session['guess_result'] = "low"
         return redirect('/greatnumbergame')
 
     elif (int(request.POST['guess']) > request.session['random']):
-        print("Too high! Try again.")
         request.session['guess_result'] = "high"
         return redirect('/greatnumbergame')
 
@@ -55,14 +51,31 @@ def reset_game(request):
 
 
 def addleader(request):
+    # Serialize datetime : credit to Gabor Szabo
+    def converter(o):
+        if isinstance(o, datetime.datetime):
+            return o.__str__()
+
+
+    leader = {
+        'player': request.POST['player'],
+        'attempts': request.session['attempts'],
+        'date': datetime.datetime.now()
+    }
+
     if ('leaderboard' not in request.session):
-        leaders = {request.POST['player']:request.session['attempts']}
+        print("not leaders yet, adding first...")
+        leaders = []
         request.session['leaderboard'] = leaders
+        request.session['leaderboard'].append(json.dumps(leader, default=converter))
     else:
-        request.session['leaderboard'][request.POST['player']] = request.session['attempts']
+        print("new leader added")
+        request.session['leaderboard'].append(json.dumps(leader, default=converter))
+        request.session.modified = True
     
     return redirect('/greatnumbergame/leaderboard')
 
 
 def leaderboard(request):
+    print(request.session['leaderboard'])
     return render(request, 'leaderboard.html')
