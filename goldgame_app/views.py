28753770c3820f474cff901ad92f76cd11b0bd0b
CepheidVariable <robertwoodard.usa@gmail.com>
2021-01-13 17:02:24 -0600

    artwork license added to HTML

--- goldgame_app/views.py
+++ goldgame_app/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def goldgame(request):
+    if ('gold' not in request.session):
+        request.session['gold'] = 0
     return render(request, 'goldgame.html')
