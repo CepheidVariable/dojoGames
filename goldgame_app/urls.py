6e26f2f02b05b4f832aa978a167e97c63b7a44f1
CepheidVariable <robertwoodard.usa@gmail.com>
2021-01-14 02:23:22 -0600

    add game settings, fixed routes, streamlined code

--- goldgame_app/urls.py
+++ goldgame_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.goldgame),
     path('earngold', views.earngold),
-
+    path('reset', views.reset),
+    path('startgame', views.startgame)
 ]
--- goldgame_app/views.py
+++ goldgame_app/views.py
@@ -5,34 +5,28 @@ import datetime
 # Create your views here.
 def goldgame(request):
     if ('gold' not in request.session):
-        log_list = []
-        request.session['gold'] = 0
-        request.session['log'] = log_list
-        request.session['gamestate'] = ""
-    
-    if (request.session['gamestate'] == 'lost'):
-        request.session['gold'] = 0
+        return render(request, 'startgame.html')
 
     return render(request, 'goldgame.html')
 
+
 def earngold(request):
     print(request.POST)
+    
     if (request.POST['action'] == "mine"):
-        net = randint(10, 20)
-        request.session['gold'] += net
+        net = randint(20, 40)
         log_data = {
             'flag': 'positive',
             'outcome': f'Earned {net} gold from the mine.',
             'date': f"({datetime.datetime.now()})"
         }
-        print(log_data)
-        request.session['log'].append(log_data)
-        request.session.modified = True
-        return redirect('/goldgame')
+        # print(log_data)
+        # request.session['log'].append(log_data)
+        # request.session.modified = True
+        # return redirect('/goldgame')
 
     elif (request.POST['action'] == "saloon"):
-        net = randint(-50, 50)
-        request.session['gold'] += net
+        net = randint(-100, 200)
 
         if (net > 0):
             outcome = "earned"
@@ -49,58 +43,90 @@ def earngold(request):
             'outcome': f'Entered a saloon and {outcome} {net} gold.',
             'date': f"({datetime.datetime.now()})"
         }
-        print(log_data)
-        request.session['log'].append(log_data)
-        request.session.modified = True
-        return redirect('/goldgame')
+        # print(log_data)
+        # request.session['log'].append(log_data)
+        # request.session.modified = True
+        # return redirect('/goldgame')
 
     elif (request.POST['action'] == "sheriff"):
-        net = randint(10, 20)
-        request.session['gold'] += net
+        net = randint(0, 100)
         log_data = {
             'flag': 'positive',
             'outcome': f'Earned {net} gold from the sheriff.',
             'date': f"({datetime.datetime.now()})"
         }
-        print(log_data)
-        request.session['log'].append(log_data)
-        request.session.modified = True
-        return redirect('/goldgame')
+        # print(log_data)
+        # request.session['log'].append(log_data)
+        # request.session.modified = True
+        # return redirect('/goldgame')
 
     elif (request.POST['action'] == "bank"):
         possible_outcomes = (-1000, -500, 0, 500, 1000)
-        net = choices(possible_outcomes, cum_weights=(.05, .25, .75, .95, 1.00), k=1)
+        temp = choices(possible_outcomes, cum_weights=(.05, .25, .75, .95, 1.00), k=1)
+        net = temp[0]
 
-        if (net[0] == 0):
+        if (net == 0):
             flag = "none"
-            outcome = f'You robbed the bank and lost your nerve half-way through. {net[0]} gold was earned.'
+            outcome = f'You robbed the bank and lost your nerve half-way through. {net} gold was earned.'
 
-        elif (net[0] == 500):
+        elif (net == 500):
             flag = "positive"
-            outcome = f'You robbed the bank and scored big! {net[0]} gold was earned.'
+            outcome = f'You robbed the bank and scored big! {net} gold was earned.'
 
-        elif (net[0] == -500):
+        elif (net == -500):
             flag = "negative"
-            outcome = f'You robbed the bank and were caught by the posse. {net[0]} gold for bail.'
+            outcome = f'You robbed the bank and were caught by the posse. {net} gold for bail.'
 
-        elif (net[0] == -1000):
+        elif (net == -1000):
             flag = "negative"
             outcome = "You robbed the bank and were shot by a pursuing posse. You died."
-            request.session['gamestate'] = 'won'
-
-        elif (net[0] == 1000):
-            flag = "positive"
-            outcome = "You hit the big-time! You retire into the sunset to live in infamy."
             request.session['gamestate'] = 'lost'
 
-        request.session['gold'] += net[0]
+        elif (net == 1000):
+            flag = "positive"
+            outcome = "You hit the big-time! You retire into the sunset to live in infamy."
+            request.session['gamestate'] = 'won'
+
         log_data = {
             'flag': flag,
             'outcome': outcome,
             'date': f"({datetime.datetime.now()})"
         }
+        
 
-        print(log_data)
-        request.session['log'].append(log_data)
-        request.session.modified = True
-        return redirect('/goldgame')
+    print(log_data)
+    request.session['gold'] += net
+    request.session['turn'] += 1
+    request.session['log'].append(log_data)
+    request.session.modified = True
+
+    if (request.session['turn_limit'] < request.session['turn']):
+        request.session['gamestate'] = "lost"
+    elif (request.session['gold'] >= request.session['gold_goal']):
+        request.session['gamestate'] = "won"
+
+    return redirect('/goldgame')
+
+
+def reset(request):
+    del request.session['gold']
+    del request.session['log']
+    del request.session['gamestate']
+    del request.session['turn']
+    del request.session['turn_limit']
+    del request.session['gold_goal']
+
+    return render(request, 'startgame.html')
+
+
+def startgame(request):
+    if ('gold' not in request.session):
+        log_list = []
+        request.session['gold'] = 0
+        request.session['log'] = log_list
+        request.session['gamestate'] = ""
+        request.session['turn'] = 0
+        request.session['turn_limit'] = int(request.POST['turns'])
+        request.session['gold_goal'] = int(request.POST['goal'])
+
+    return redirect('/goldgame')
