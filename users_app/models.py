29c4aad4a8c258a92a06d7c9d9356eada8e6048b
CepheidVariable <robertwoodard.usa@gmail.com>
2021-01-19 21:44:42 -0600

    added registration/login forms on HTML/CSS

--- users_app/models.py
+++ users_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import re
+from datetime import datetime
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=25, unique=True)
+    first_name = models.CharField(max_length=35)
+    last_name = models.CharField(max_length=35)
+    email = models.CharField(max_length=254, unique=True)
+    dob = models.DateField()
+    password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
